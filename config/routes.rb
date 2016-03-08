9b8f745bd5d92e03ee90d5d5b6db95e529a2702d
Charlie <sgvvannabe@gmail.com>
2016-03-08 10:16:25 +0000

    Building the User Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
   #'controller#method'
-  
   resources :places do
     resources :comments, only: :create
   end
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
